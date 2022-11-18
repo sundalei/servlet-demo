d5a6bf0141e233bb9b857e7a18ba430b462399ef
DaLei Sun <sundalei1988@gmail.com>
2022-11-18 15:05:37 +0800

    servlet context demo

--- servlet-context-demo/src/main/java/com/wileyedge/ContextServlet.java
+++ servlet-context-demo/src/main/java/com/wileyedge/ContextServlet.java
@@ -1,11 +1,22 @@
 package com.wileyedge;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/context")
 public class ContextServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
 
 }
