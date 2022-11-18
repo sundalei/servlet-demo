4efc63ea420b72625f6bcb275a076e5fdd58fed3
DaLei Sun <sundalei1988@gmail.com>
2022-11-18 17:56:21 +0800

    context demo

--- servlet-context-demo/src/main/java/com/wileyedge/ContextServlet.java
+++ servlet-context-demo/src/main/java/com/wileyedge/ContextServlet.java
@@ -2,6 +2,7 @@ package com.wileyedge;
 
 import java.io.IOException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,8 +16,21 @@ public class ContextServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		
+		ServletContext servletContext = getServletContext();
+		
+		// Resource associated with the web context
+		String contextPath = servletContext.getContextPath();
+		String realPath = servletContext.getRealPath("."); // webapps folder
+		
+		// Object-valued attributes
+		servletContext.setAttribute("username", "test");
+		
+		// Logging capabilities
+		servletContext.log("This is a log message");
+		
+		System.out.println("contextPath: " + contextPath);
+		System.out.println("realPath: " + realPath);
 	}
 
 }
